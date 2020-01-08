b69d2f7433e4ba9e86d241177da398516fcb5aee
AdmiralYuuShi <hapidmochj12@gmail.com>
2020-01-08 16:45:56 +0700

    Add wishlist

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const product = require('./routes/product')
 const auth = require('./routes/auth')
 const cart = require('./routes/cart')
 const customers = require('./routes/customers')
+const wishlist = require('./routes/wishlist')
 
 Route
   .use('/product', product)
   .use('/auth', auth)
   .use('/customers', customers)
   .use('/cart', cart)
+  .use('/wishlist', wishlist)
 
 module.exports = Route
