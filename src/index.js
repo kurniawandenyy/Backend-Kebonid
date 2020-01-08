b2b881d3ba95479d9e7c341434339565d55f31d6
AdmiralYuuShi <hapidmochj12@gmail.com>
2020-01-08 15:02:12 +0700

    Add cart

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const Route = express.Router()
 
 const product = require('./routes/product')
 const auth = require('./routes/auth')
+const cart = require('./routes/cart')
 const customers = require('./routes/customers')
 
 Route
   .use('/product', product)
   .use('/auth', auth)
   .use('/customers', customers)
+  .use('/cart', cart)
 
 module.exports = Route
