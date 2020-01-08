3cb61858ccbbd1ddae110dc7a068b14067121197
alfatahh54 <alfatah215@gmail.com>
2020-01-08 10:42:18 +0700

    CRUD product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const Route = express.Router()
+const todoList = require('../controllers/product')
 Route
-  .get('/', (req, res) => res.status(200).json({
-    status: 201,
-    message: 'OK'
-  }))
+  .get('/', todoList.getProduct)
+  .get('/:id', todoList.getProductById)
+  .post('/', todoList.createProduct)
+  .patch('/:id', todoList.updateProduct)
+  .delete('/:id', todoList.deleteProduct)
+
 module.exports = Route
