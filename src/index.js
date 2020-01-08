b054dc3326bb1487bb7c88882ebb906a3a68a465
kurniawandenyy <denykurniawan146@gmail.com>
2020-01-08 15:01:27 +0700

    add get and create message

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const product = require('./routes/product')
 const auth = require('./routes/auth')
 const customers = require('./routes/customers')
 const transaction = require('./routes/transaction')
+const message = require('./routes/message')
 
 Route
   .use('/transaction', transaction)
   .use('/product', product)
   .use('/auth', auth)
   .use('/customers', customers)
+  .use('/message', message)
 
 module.exports = Route
