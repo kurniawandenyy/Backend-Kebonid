ba60ea6cce6bac182657131779e7305d1ff4da3d
alfatahh54 <alfatah215@gmail.com>
2020-01-08 13:40:38 +0700

    Create Store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const Route = express.Router()
 
 const product = require('./routes/product')
-
+const store = require('./routes/store')
 Route
   .use('/product', product)
+  .use('/store', store)
 
 module.exports = Route
